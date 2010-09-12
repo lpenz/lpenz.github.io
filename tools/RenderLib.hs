4a09aa4c18805477da52214d09112ae6f0b35fdc
Leandro Lisboa Penz <lpenz@lpenz.org>
2010-09-11 21:16:40 -0300

    Initial breadcrumbs development.

--- tools/RenderLib.hs
+++ tools/RenderLib.hs
@@ -14,7 +14,18 @@ getFileTitle src = do
     return $ head $ lines srct2t
 
 
+getBreadcrumbs :: FilePath -> IO (String)
+getBreadcrumbs src = do
+    let p = init $ splitPath src
+    putStrLn $ show p
+    -- mapM gettitles p
+    -- where
+    --     gettitles f = do
+    --         e <- fileExists (f ++ "index.html")
+    --         if e then return (getfileTitle
+    return ""
 
+		-- <a href="$top$/index.html">Avulsos by Penz</a>
 
 renderT2T :: (StringTemplate String -> StringTemplate String) -> FilePath -> FilePath -> FilePath -> IO ()
 renderT2T attrs tpfile src dst = do
@@ -23,11 +34,13 @@ renderT2T attrs tpfile src dst = do
     tp0 <- liftM newSTMP $ readFile tpfile
     let lvl = length (splitPath src) - 1
     let top = if lvl == 0 then "." else (concat $ intersperse "/" $ replicate lvl "..")
+    breadcrumbs <- getBreadcrumbs src
     let tp =
             attrs
             $ setAttribute "top" top
             $ setAttribute "title" title
             $ setAttribute "content" srchtml
+            $ setAttribute "breadcrumbs" breadcrumbs
             $ tp0
     let (err, _, _) = checkTemplate tp
     when (isJust err) $ error $ fromJust err
