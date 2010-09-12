4311afae8d44d02373bc48c8d1492622cffa80bf
Leandro Lisboa Penz <lpenz@lpenz.org>
2010-09-11 21:05:51 -0300

    Check for errors in template.

--- tools/RenderLib.hs
+++ tools/RenderLib.hs
@@ -5,26 +5,32 @@ import System.Process
 import System.FilePath
 import Data.List
 import Control.Monad
+import Data.Maybe
 
 
-getTitle :: FilePath -> IO (String)
-getTitle src = do
+getFileTitle :: FilePath -> IO (String)
+getFileTitle src = do
     srct2t  <- readFile src
     return $ head $ lines srct2t
 
 
+
+
 renderT2T :: (StringTemplate String -> StringTemplate String) -> FilePath -> FilePath -> FilePath -> IO ()
 renderT2T attrs tpfile src dst = do
-    title <- getTitle src
+    title <- getFileTitle src
     srchtml <- readProcess "/usr/bin/txt2tags" [ "-t", "html", "-H", "-i", src, "-o", "-" ] []
-    tp <- liftM newSTMP $ readFile tpfile
+    tp0 <- liftM newSTMP $ readFile tpfile
     let lvl = length (splitPath src) - 1
     let top = if lvl == 0 then "." else (concat $ intersperse "/" $ replicate lvl "..")
-    writeFile dst $ toString
-        $ attrs
-        $ setAttribute "top" top
-        $ setAttribute "title" title
-        $ setAttribute "content" srchtml
-        $ tp
+    let tp =
+            attrs
+            $ setAttribute "top" top
+            $ setAttribute "title" title
+            $ setAttribute "content" srchtml
+            $ tp0
+    let (err, _, _) = checkTemplate tp
+    when (isJust err) $ error $ fromJust err
+    writeFile dst $ toString tp
 
 
