2f894b71b0396797c42bc1d0db6511e06f3dd05a
Leandro Lisboa Penz <lpenz@lpenz.org>
2010-11-01 18:56:41 -0200

    Rendering home and title.

--- tools/render.py
+++ tools/render.py
@@ -4,6 +4,7 @@ import SCons.Builder
 import SCons.Util
 import SCons.Scanner
 import SCons.Tool
+import yaml
 import os
 import re
 
@@ -26,7 +27,15 @@ def renderSourceScanner(node, env, path):
 
 
 def renderTargetScanner(node, env, path):
-    return [os.path.join(env['TOP'], env['RENDER'])]
+    rv = [os.path.join(env['TOP'], env['RENDER'])]
+    yfilename = os.path.join(os.path.dirname(str(node)), 'info.yaml')
+    if os.path.isfile(yfilename):
+        y = yaml.load(open(yfilename).read())
+        if y.has_key('template'):
+            rv.append(os.path.join(env['TOP'], 'templates', y['template'] + '.html'))
+        else:
+            rv.append(os.path.join(env['TOP'], 'templates', 'base.html'))
+    return rv
 
 
 def generate(env):
