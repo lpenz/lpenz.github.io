c5d86757c04d1021049d3b157553ca1a37cde4bd
Leandro Lisboa Penz <lpenz@lpenz.org>
2010-12-12 20:41:24 -0200

    df0pred-1: totalspace var; units of 1MB; improved text.

--- articles/df0pred-1/datain.R
+++ articles/df0pred-1/datain.R
@@ -1,2 +1,3 @@
 duinfo <- read.table('duinfo.dat', colClasses=c("Date","numeric"), col.names=c("day","usd"))
 attach(duinfo)
+totalspace <- 200000
--- articles/df0pred-1/main.R
+++ articles/df0pred-1/main.R
@@ -18,13 +18,13 @@ plot(usd ~ day)
 abline(model)
 
 sink(file='predict.txt')
-cat('> predict(model2, data.frame(usd = 1e9))\n')
-predict(model2, data.frame(usd = 1e9))
+cat('> predict(model2, data.frame(usd = totalspace))\n')
+predict(model2, data.frame(usd = totalspace))
 sink()
 
 sink(file='predictdate.txt')
-cat('> as.Date(predict(model2, data.frame(usd = 1e9)), origin="1970-01-01")\n')
-as.Date(predict(model2, data.frame(usd = 1e9)), origin="1970-01-01")
+cat('> as.Date(predict(model2, data.frame(usd = totalspace)), origin="1970-01-01")\n')
+as.Date(predict(model2, data.frame(usd = totalspace)), origin="1970-01-01")
 sink()
 
 sink(file='lmrandsummary.txt')
