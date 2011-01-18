ab397695442601137b8d43685e55ca215ce1571b
Leandro Lisboa Penz <lpenz@lpenz.org>
2011-01-17 23:04:23 -0200

    df0pred-2: using better data also.
    Still need to analyse spike; let's try to make the equivalent of df0pred-1
    for now.

--- articles/df0pred-2/datain.R
+++ articles/df0pred-2/datain.R
@@ -3,6 +3,6 @@ duinfo <- read.table('duinfospike.dat',
 		colClasses=c("Date","numeric"),
 		col.names=c("day","usd"))
 attach(duinfo)
-totalspace <- 1e6
+totalspace <- 500000
 today <- day[length(day)]
 
