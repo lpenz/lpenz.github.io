5279acb6f168a505f78e5136a89e245416c67650
Leandro Lisboa Penz <lpenz@lpenz.org>
2011-01-22 16:48:51 -0200

    df0pred-2: data improved.
    We now have a linear model that would drive into infinity.

--- articles/df0pred-2/df0densityplot.R
+++ articles/df0pred-2/df0densityplot.R
@@ -1,2 +1,2 @@
 plot(dens, xaxt='n')
-axis.Date(1, densday, at=seq(min(densdays), max(densdays), 'month'), format='%F')
+axis.Date(1, densday, at=seq(min(densdays), max(densdays), 'year'), format='%F')
