42727c4eb427e2b5b5ec5bb6a93c187d12013b3d
Leandro Lisboa Penz <lpenz@lpenz.org>
2011-05-08 19:04:53 -0300

    Unused file deleted.

--- mcsdayplot.R
+++
@@ -1,4 +0,0 @@
-plot(dssim[,1], xlab='simulation', ylab='usd', col='blue')
-points(dssim[,10], col='brown')
-points(dssim[,100], col='green')
-legend('top', c('1 day', '10 days', '100 days'), fill=c('blue', 'brown', 'green'))
