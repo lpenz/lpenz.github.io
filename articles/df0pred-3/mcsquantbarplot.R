12206050b3a843d64581406b870be49de84133ac
Leandro Lisboa Penz <lpenz@lpenz.org>
2011-05-08 19:33:17 -0300

    Quantile plots fixed, color problems.

--- articles/df0pred-3/mcsquantbarplot.R
+++ articles/df0pred-3/mcsquantbarplot.R
@@ -1,10 +1,11 @@
-q <- 6
+q <- 7
 f <- function(i) {
-	qa <- quantile(dssim[,i], seq(0, 1, 1.0/(q-1)))
-	c(qa[1], mapply(function(j) qa[j] - qa[j-1], seq(2, q)))
+	qa <- quantile(dssim[,i], seq(0, 1, 1.0/q))
+	c(qa[1], mapply(function(j) qa[j] - qa[j-1], seq(2, q+1)))
 }
 qvals <- mapply(f, seq(1, numdays+1))
-colors <- c('white', rainbow(q/2), tail(rev(rainbow(q/2)), -1))
+colors0 <- rainbow((1+q)/2)
+colors <- c('white', colors0, rev(if (q %% 2 == 0) colors0 else head(colors0, -1)))
 barplot(qvals, ylab='usd', xaxt='n', col=colors, ylim=c(min(dssim), max(dssim)), border=NA, space=0)
 axis.Date(1, day, at=seq(min(fday), max(fday), 'week'), format='%F')
 lines(seq(0, numdays), replicate(numdays+1, totalspace), col='gray')
--- articles/df0pred-3/mcsquantplot.R
+++ articles/df0pred-3/mcsquantplot.R
@@ -1,8 +1,9 @@
-q <- 7
-f <- function(i) quantile(dssim[,i], seq(0, 1, 1.0/(q-1)))
+q <- 6
+f <- function(i) quantile(dssim[,i], seq(0, 1, 1.0/q))
 qvals <- mapply(f, seq(1, numdays+1))
-colors <- c(rainbow(1+q/2), tail(rev(rainbow(1+q/2)), -1))
+colors0 <- rainbow(1+q/2)
+colors <- c(colors0, rev(if (q %% 2) colors0 else head(colors0, -1)))
 plot(fday, qvals[1,], ylab='usd', xaxt='n', type='l', col=colors[1], ylim=c(min(qvals), max(qvals)))
-mapply(function(i) lines(fday, qvals[i,], col=colors[i]), seq(2, q))
+mapply(function(i) lines(fday, qvals[i,], col=colors[i]), seq(2, q+1))
 axis.Date(1, day, at=seq(min(fday), max(fday), 'week'), format='%F')
 lines(seq(0, numdays), replicate(numdays+1, totalspace), col='gray')
