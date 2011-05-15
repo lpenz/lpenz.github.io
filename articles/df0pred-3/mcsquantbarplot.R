968f01a03acf0697b2886c7db543caa723b38981
Leandro Lisboa Penz <lpenz@lpenz.org>
2011-05-14 22:38:58 -0300

    Small improvements in text and graphs.
    Max line, labels, etc.

--- articles/df0pred-3/mcsquantbarplot.R
+++ articles/df0pred-3/mcsquantbarplot.R
@@ -4,8 +4,7 @@ f <- function(i) {
 	c(qa[1], mapply(function(j) qa[j] - qa[j-1], seq(2, q+1)))
 }
 qvals <- mapply(f, seq(1, numdays+1))
-colors0 <- rainbow((1+q)/2)
+colors0 <- rev(rainbow((1+q)/2))
 colors <- c('white', colors0, rev(if (q %% 2 == 0) colors0 else head(colors0, -1)))
-barplot(qvals, ylab='usd', xaxt='n', col=colors, ylim=c(min(dssim), max(dssim)), border=NA, space=0)
-axis.Date(1, day, at=seq(min(fday), max(fday), 'week'), format='%F')
-lines(seq(0, numdays), replicate(numdays+1, totalspace), col='gray')
+barplot(qvals, ylab='usd', xlab='day', names.arg=seq(min(fday), max(fday), 'day'), col=colors, ylim=c(min(dssim), max(dssim)), border=NA, space=0)
+abline(h=totalspace, col='gray')
--- articles/df0pred-3/mcsquantplot.R
+++ articles/df0pred-3/mcsquantplot.R
@@ -1,9 +1,9 @@
 q <- 6
 f <- function(i) quantile(dssim[,i], seq(0, 1, 1.0/q))
 qvals <- mapply(f, seq(1, numdays+1))
-colors0 <- rainbow(1+q/2)
+colors0 <- rev(rainbow(1+q/2))
 colors <- c(colors0, rev(if (q %% 2) colors0 else head(colors0, -1)))
-plot(fday, qvals[1,], ylab='usd', xaxt='n', type='l', col=colors[1], ylim=c(min(qvals), max(qvals)))
+plot(fday, qvals[1,], ylab='usd', xlab='day', xaxt='n', type='l', col=colors[1], ylim=c(min(qvals), max(qvals)))
 mapply(function(i) lines(fday, qvals[i,], col=colors[i]), seq(2, q+1))
 axis.Date(1, day, at=seq(min(fday), max(fday), 'week'), format='%F')
-lines(seq(0, numdays), replicate(numdays+1, totalspace), col='gray')
+abline(h=totalspace, col='gray')
