62dfdc0a7be949c4868576397912e8202c5f7c48
Leandro Lisboa Penz <lpenz@lpenz.org>
2011-08-09 22:00:12 -0300

    Balls are now green and red.

--- articles/bugprobhunt/pdistplot.R
+++ articles/bugprobhunt/pdistplot.R
@@ -6,11 +6,11 @@ library('ggplot2', quietly=TRUE)
 a <- commandArgs(TRUE)
 
 red    <- as.integer(a[1])
-white  <- as.integer(a[2])
+green  <- as.integer(a[2])
 output <- a[3]
 
 k <- red
-n <- red+white
+n <- red+green
 
 x <- seq(0, 1, 0.001)
 y <- dbeta(x, k+1, n-k+1) / (100 * beta(k+1, n-k+1))
