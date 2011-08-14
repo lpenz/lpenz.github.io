0180333949992e2c3b27acfcbb4c4cb2f74d4da0
Leandro Lisboa Penz <lpenz@lpenz.org>
2011-08-14 19:40:27 -0300

    Finished "Increasing n", started next chapter.
    - Finally understood density.
    - Scripts reorganized.

--- articles/bugprobhunt/pdistplot.R
+++ articles/bugprobhunt/pdistplot.R
@@ -12,10 +12,14 @@ output <- a[3]
 k <- red
 n <- red+green
 
-x <- seq(0, 1, 0.001)
-y <- dbeta(x, k+1, n-k+1) / (100 * beta(k+1, n-k+1))
+x <- seq(0, 1, 0.01)
+df <- data.frame(x=x, y=dbinom(k, n, x))
 
 png(output, width=800)
-qplot(x, y, geom='line', xlab='p', ylab='P(p)', main=paste('p distribution for ', red, ' red and ', green, ' green balls'))
-dev.off()
+ggplot(df, aes(x = x, y = y)) +
+	opts(title=paste('p distribution for', red, 'red and', green, 'green balls')) +
+	scale_x_continuous(name='p') +
+	scale_y_continuous(name='P(k=1, n=4, p=p)') +
+	geom_point(size=2) + geom_linerange(aes(ymin = 0, ymax = y))
+#qplot(x, y, geom='line', xlab='p', ylab='P(p)', main=paste('p distribution for', red, 'red and', green, 'green balls'))
 
