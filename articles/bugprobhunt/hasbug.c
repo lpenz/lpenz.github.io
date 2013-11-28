d607aed3c7a1bec6520d33a255084f3d85c12218
Leandro Lisboa Penz <lpenz@lpenz.org>
2013-11-28 19:20:17 -0200

    bugprobhunt: hasbug with really 1/4 chance (I hope).

--- articles/bugprobhunt/hasbug.c
+++ articles/bugprobhunt/hasbug.c
@@ -2,32 +2,35 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int get_random_short(short *r)
+/* Returns -1 if error, other number if ok. */
+int get_random_chars(char *r1, char*r2)
 {
 	int f = open("/dev/urandom", O_RDONLY);
-	char ret = 0;
-	short c;
 
 	if (f < 0)
 		return -1;
-
-	if (read(f, &c, sizeof(&c)) < 0)
+	if (read(f, r1, sizeof(*r1)) < 0)
 		return -1;
-
-	*r = c;
+	if (read(f, r2, sizeof(*r2)) < 0)
+		return -1;
 	close(f);
 
-	return ret % 4;
+	return *r1 & *r2;
 }
 
 int main(void)
 {
-	short r;
-	int ret = get_random_short(&r);
+	char r1;
+	char r2;
+	int ret;
+
+	ret = get_random_chars(&r1, &r2);
+
 	if (ret < 0)
-		printf("error\n");
+		fprintf(stderr, "error");
 	else
-		printf("%d\n", r);
-	return 0;
+		printf("%d %d\n", r1, r2);
+
+	return ret < 0;
 }
 
