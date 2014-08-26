06d1bef23cb22c6086841765d245821474dcd5b8
Leandro Lisboa Penz <lpenz@lpenz.org>
2014-08-25 22:01:09 -0300

    df0-1: generate main.RData as the other.
    So that we always use the same data and generate the same pics.

--- articles/df0pred-1/main.R
+++ articles/df0pred-1/main.R
@@ -2,34 +2,5 @@
 
 source('init.R')
 
-png('pointplot.png')
-plot(usd ~ day, xaxt='n')
-axis.Date(1, day, format='%F')
-
-sink(file='lm.txt')
-model
-sink()
-
-sink(file='lmsummary.txt')
-summary(model)
-sink()
-
-png('lmplot.png')
-plot(usd ~ day, xaxt='n')
-axis.Date(1, day, format='%F')
-abline(model)
-
-sink(file='predict.txt')
-cat('> predict(model2, data.frame(usd = totalspace))\n')
-predict(model2, data.frame(usd = totalspace))
-sink()
-
-sink(file='predictdate.txt')
-cat('> as.Date(predict(model2, data.frame(usd = totalspace)), origin="1970-01-01")\n')
-as.Date(predict(model2, data.frame(usd = totalspace)), origin="1970-01-01")
-sink()
-
-sink(file='lmrandsummary.txt')
-summary(lm(drand$usd ~ drand$day))
-sink()
+save(list=c('totalspace','day','usd','model','model2','drand'), file='main.RData')
 
